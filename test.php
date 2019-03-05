a9fc3cea015e5b77b40b9ebf4d122ac3f6e3b4ee
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2019-03-05 23:29:39 +0100

    Beginning to test parse.php, non-recursive

--- test.php
+++ test.php
@@ -37,30 +37,6 @@ class Parameters {
     }
 
     /**
-     * Sets filename of script for analysis
-     * @param $filename New filename
-     */
-    function setParseFile($filename) {
-        $this->parseFile = $filename;
-    }
-
-    /**
-     * Sets filename of script for interpret
-     * @param $filename New filename
-     */
-    function setIntFile($filename) {
-        $this->intFile = $filename;
-    }
-
-    /**
-     * Sets directory where tests will be located
-     * @param $dirname New directory
-     */
-    function setDirectory($dirname) {
-        $this->directory = $dirname;
-    }
-
-    /**
      * Increase number of times option was input
      * @param $option Name of option
      */
@@ -122,14 +98,51 @@ class Parameters {
      */
     function checkOptionsWithoutConflicts() {
         // Check if any option is > 1
+        foreach ($this->options as $key => $value) {
+            if ($value > 1) {
+                error_log("Error! Some of parameters were input multiple times.");
+                return false;
+            }
+        }
 
         // Check if --help option is with anything else
+        if ($this->options["help"] == 1) {
+            foreach ($this->options as $key => $value)
+                if ($key != "help" && $value > 0) {
+                    error_log("Error! Cannot have --help with additional parameters.");
+                    return false;
+                }
+        }
 
         // Check if --parse-script with --int-only
+        if ($this->options["parse-script"] == 1 && $this->options["int-only"] == 1) {
+            error_log("Error! Cannot have --parse-script and --int-only parameters at the same time.");
+            return false;
+        }
 
         // Check if --int-script with --parse-only
+        if ($this->options["int-script"] == 1 && $this->options["parse-only"] == 1) {
+            error_log("Error! Cannot have --int-script and --parse-only parameters at the same time.");
+            return false;
+        }
 
-        return true;
+        return true; // No error - OK
+    }
+
+    /**
+     * Decide what actions to take based on selected options.
+     * Expects that method checkProgramArguments and checkOptionsWithoutConflicts were called prior.
+     */
+    function decideCourseOfAction() {
+        if ($this->options["help"] == 1) {
+            display_help();
+        }
+        elseif ($this->options["parse-only"] == 1) {
+
+        }
+        elseif ($this->options["int-only"] == 1) {
+
+        }
     }
 }
 
@@ -156,13 +169,120 @@ Arguments:
 EOL;
 }
 
+/**
+ * @param $path Full path to file
+ * @param $content Content to put in file
+ * @return int Error ok - ERR_OK or ERR_OUTPUT_FILES
+ */
+function generate_file($path, $content) {
+    $handle = fopen($path, "w");
+    if ($handle == false) {
+        error_log("Error! Could not create " . $path . ".\n");
+        return ERR_OUTPUT_FILES;
+    }
+    print("I am going to write in file " . $path . " now.\n");
+
+    fwrite($handle, $content);
+
+    fclose($handle);
+    return ERR_OK;
+}
+
+/**
+ * Automatic test only for parse, non-recursive option.
+ * @param $directory Directory with test
+ * @param $parseFile Filename of parse script
+ * @return int Error code
+ */
+function test_parse_non_recursive($directory, $parseFile) {
+    $files = scandir($directory);
+    foreach ($files as $file) {
+        if (is_file($directory . "/" . $file) && (preg_match("/.src/", $file))) {
+
+            $name = substr($file, 0, strlen($file) - 4);
+            print("-- filename without .src: " . $name . "\n");
+
+            // Check if it has .rc file
+            if (!is_file($directory . "/" . $name . ".rc")) {
+                print("--- also does not have .rc file Generate it!\n");
+                generate_file($directory . "/" . $name . ".rc", "0\n");
+            }
+            // Check if it has .out file
+            if (!is_file($directory . "/" . $name . ".out")) {
+                print("--- also does not have .out file Generate it!\n");
+                generate_file($directory . "/" . $name . ".out", "");
+            }
+            // Check if it has .in file
+            if (!is_file($directory . "/" . $name . ".in")) {
+                print("--- also does not have .in file Generate it!\n");
+                generate_file($directory . "/" . $name . ".in", "");
+            }
+
+            // We now have all necessary file - do the test!
+            // Run parse.php (or whatever it is called) with current source file and save output to .my_out and return code to .my_rc
+            exec("php7.3 " . $parseFile . " < " . ($directory . "/" . $file) . " > " . ($directory . "/" . $name . ".my_out") );
+            exec("echo $? > " . $directory . "/" . $name . ".my_rc");
+
+            // Compare return codes
+            // TODO - diff nech ignoruje prázdne riadky
+            $rc_diff = shell_exec("diff -w " . $directory . "/" . $name . ".my_rc " . $directory . "/" . $name . ".rc");
+            //print("diff --strip-trailing-cr " . $directory . "/" . $name . ".my_rc " . $directory . "/" . $name . ".rc\n");
+            print("rc diff: " . $rc_diff . "\n");
+            // Compare outputs
+            //
+        }
+    }
+
+    return ERR_OK;
+}
+
 # Main body
 
+/*
 $params = new Parameters();
 
 // Check provided arguments
 if ($params->checkProgramArguments() != ERR_OK)
     exit(ERR_SCRIPT_PARAMS);
+// Check for conflicts between parameters
+if(!$params->checkOptionsWithoutConflicts())
+    exit(ERR_SCRIPT_PARAMS);
 
-var_dump($params);
+$params->decideCourseOfAction();
+*/
+
+$parseFile = "parse.php";
+$directory = "mytests";
+
+// TODO - return error code if directory does not exist
+
+if (is_file($parseFile))
+    print("file exists\n");
+else
+    print("file does not exit\n");
+
+if (is_dir($directory ))
+    print("is directory\n");
+else
+    print("isn't directory\n");
+
+/*
+$current_path = $directory;
+
+$files = scandir($directory);
+var_dump($files);
+foreach ($files as $file) {
+    if (is_file($current_path . "/" . $file) && (preg_match("/.src/", $file))) {
+        print("- is src file: " . $current_path . "/" . $file . "\n");
+
+        $name = substr($file, 0, strlen($file) - 4);
+        print("-- filename without .src: " . $name . "\n");
+        // Check if it has .rc file
+        if (is_file($current_path . "/" . $name . ".out"))
+            print("--- also has .out file.\n");
+    }
+}*/
+
+test_parse_non_recursive($directory, $parseFile);
+
 ?>
