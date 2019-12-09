2d2c978d4e3ee9f901700366e9b933ccf7868166
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2019-12-09 22:19:24 +0100

    Starting from scratch, port, timer init

--- main.c
+++ main.c
@@ -14,103 +14,16 @@
 /* Macros for bit-level registers manipulation */
 #define GPIO_PIN_MASK 0x1Fu
 #define GPIO_PIN(x) (((1)<<(x & GPIO_PIN_MASK)))
-
 #define SPK 0x10          // Speaker is on PTA4
+#define OVERFLOW 0xFF // Doporucena hodnota preteceni casovace
 
-enum tone_e {
-	c1 = 261,
-	c1S = 277,
-	d1 = 294,
-	d1S = 311,
-	e1 = 329,
-	f1 = 349,
-	f1S = 370,
-	g1 = 391,
-	g1S = 415,
-	a1 = 440,
-	a1S = 455,
-	b1 = 466,
-	h1 = 493,
-	c2 = 523,
-	c2S = 554,
-	d2 = 587,
-	d2S = 622,
-	e2 = 659,
-	f2 = 698,
-	f2S = 740,
-	g2 = 784,
-	g2S = 830,
-	a2 = 880
-} Tone;
-
-int n=0;								// index
-unsigned char c;						// prijaty znak
-unsigned char prompt[7] = "Login>";		// text vyzvy
-unsigned char corrl[9] = "xholko02";		// Vas login
-unsigned char login[9];					// misto pro prijaty retezec
-int pressed_up = 0, pressed_down = 0;
-int beep_flag = 0;
-unsigned int compare = 0x200;
-
-
-/* Initialize the MCU - basic clock settings, turning the watchdog off */
-void MCUInit(void)  {
-    MCG_C4 |= ( MCG_C4_DMX32_MASK | MCG_C4_DRST_DRS(0x01) );
-    SIM_CLKDIV1 |= SIM_CLKDIV1_OUTDIV1(0x00);
-    WDOG_STCTRLH &= ~WDOG_STCTRLH_WDOGEN_MASK;
-}
-
-void PortsInit(void) {
-    /* Turn on all port (A, B, E) and UART5 clocks */
-    SIM->SCGC5 = SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTE_MASK | SIM_SCGC5_PORTA_MASK;
-	SIM->SCGC1 = SIM_SCGC1_UART5_MASK;
-
-    PORTE->PCR[8] = PORT_PCR_MUX(0x03); // UART0_TX
-    PORTE->PCR[9] = PORT_PCR_MUX(0x03); // UART0_RX
-
-    PORTA->PCR[4] = PORT_PCR_MUX(0x01);  // Speaker
-
-    /* Change corresponding PTB port pins as outputs */
-    PTA->PDDR = GPIO_PDDR_PDD(SPK);     // Speaker as output
-}
-
-/* Initialize UART */
-void UART5Init() {
-	UART5->C2 &= ~(UART_C2_TE_MASK | UART_C2_RE_MASK);
-	UART5->BDH = 0x00;
-	UART5->BDL = 0x1A; // Baud rate 115 200 Bd, 1 stop bit
-	UART5->C4 = 0x0F; // Oversampling ratio 16, match address mode disabled
-	UART5->C1 = 0x00; // 8 data bitu, bez parity
-	UART5->C3 = 0x00;
-	UART5->MA1 = 0x00; // no match address (mode disabled in C4)
-	UART5->MA2 = 0x00; // no match address (mode disabled in C4)
-	UART5->S2 |= 0xC0; // ?
-	UART5->C2 |= (UART_C2_TE_MASK | UART_C2_RE_MASK); // ?
-}
-
-void SendCh(char ch)  {
-	while(!(UART5->S1 & UART_S1_TDRE_MASK) && !(UART5->S1 & UART_S1_TC_MASK) );
-	UART5->D = ch;
-}
-
-char ReceiveCh(void) {
-	while(!(UART5->S1 & UART_S1_RDRF_MASK));
-	return UART5->D;
-}
-
-void SendStr(char *s)  {
-	int i = 0;
-	while (s[i]!=0) {
-		SendCh(s[i++]);
-	}
-}
-
+/* A delay function */
 void delay(long long bound) {
+
   long long i;
   for(i=0;i<bound;i++);
 }
 
-/* Pipnuti pres bzucak - generuje 500 period obdelnikoveho signalu */
 void beep(void) {
     for (unsigned int q=0; q<500; q++) {
         PTA->PDOR = GPIO_PDOR_PDO(0x0010);
@@ -120,91 +33,58 @@ void beep(void) {
     }
 }
 
-/* freq - frequency, duration - duration in microseconds? */
-void playNote(int note, int duration) {
-
-	long dl = (long)(10000/note);
-	long time = (long)((duration*100)/(dl*2));
-	for (int i=0;i<time;i++)
-	{
-		GPIOA_PDOR ^= SPK; // Speaker switched on
-		delay(dl*55);   //
-	}
-	delay(800);
+/* Initialize the MCU - basic clock settings, turning the watchdog off */
+void MCUInit(void)  {
+    MCG_C4 |= ( MCG_C4_DMX32_MASK | MCG_C4_DRST_DRS(0x01) );
+    SIM_CLKDIV1 |= SIM_CLKDIV1_OUTDIV1(0x00);
+    WDOG_STCTRLH &= ~WDOG_STCTRLH_WDOGEN_MASK;
 }
 
-void PlayNote(int freq, int duration) {
-	long half_period = (long) (10000 / freq); // x10000 to get int
-	long time = (long) ((duration * 100) / (half_period));
-	for (int i=0; i < time; i++) {
-//		GPIOA_PDOR ^= SPK; // Speaker switched on
-        PTA->PDOR = GPIO_PDOR_PDO(0x0010);
-		delay(half_period * 30);   //
-		PTA->PDOR = GPIO_PDOR_PDO(0x0000);
-		delay(half_period * 30);   //
-	}
-	delay(800);
+/* Povoleni hodin do modulu, ktere budou pouzivany */
+void SysInit(void) {
+	// Defaultne je FTM clocked by internal vus clock
+	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK; // povoleni hodin do portu B (pro RGB LED)
+	SIM->SCGC6 |= SIM_SCGC6_FTM0_MASK; // povoleni hodin do casovace FTM0
 }
 
-void PlaySong1() //Imperial march
+void PortsInit(void)
 {
-    PlayNote(a1, 500);
-    PlayNote(a1, 500);
-    PlayNote(a1, 500);
-    PlayNote(f1, 350);
-    PlayNote(c2, 150);
-    PlayNote(a1, 500);
-    PlayNote(f1, 350);
-    PlayNote(c2, 150);
-    PlayNote(a1, 650);
+    /* Turn on all port clocks */
+    SIM->SCGC5 = SIM_SCGC5_PORTA_MASK;
 
-    delay(3000);
-
-    PlayNote(e2, 500);
-    PlayNote(e2, 500);
-    PlayNote(e2, 500);
-    PlayNote(f2, 350);
-    PlayNote(c2, 150);
-    PlayNote(g1S, 500);
-    PlayNote(f1, 350);
-    PlayNote(c2, 150);
-    PlayNote(a1, 650);
-
-    delay(3000);
-
-    PlayNote(a1, 500);
-    PlayNote(a1, 300);
-    PlayNote(a1, 150);
-    PlayNote(a2, 400);
-    PlayNote(g2S, 200);
-    PlayNote(g2, 200);
-    PlayNote(f2S, 125);
-    PlayNote(f2, 125);
-    PlayNote(f2S, 250);
-
-    delay(3000);
+    PORTA->PCR[4] = PORT_PCR_MUX(0x01);  // Speaker
+    PTA->PDDR = GPIO_PDDR_PDD(SPK);     // Speaker as output
 }
 
-int main(void) {
+void TimerInit(void) {
+	// Vynulovaù register ËÌtaËa
+	FTM0_CNT = 0x0;
+
+	// Nastaviù hodnotu preteËenia do modulo registru TPMO
+	//FTM0_MOD = OVERFLOW;
+	FTM0_MOD = 0xFF;
+
+	// Nastaviù reûim generovania PWM na zvolenom kan·le (n) ËasovaËa
+	// v riadiacom registri FTMx_CnSC
+	// Edge-aligned PWM: High-true pulses (clear Output on match, set Output on reload),
+    // preruöenie ani DMA requests nebud˙ vyuûÌvanÈ.
+	FTM0_C0SC = 0x28;
+
+	// Nastaviù konfigur·ciu ËasovaËa v jeho stavovem a ridicim registru (SC):
+	// (up counting mode pre Edge-aligned PWM, Clock Mode Selection (01),
+	// Prescale Factor Selection (Divide by 8), bez vyuûitia preruöenia Ëi DMA.
+	FTM0_SC = 0xB;
+}
+
+int main(void)
+{
     MCUInit();
+    SysInit();
     PortsInit();
-    UART5Init();
+    TimerInit();
 
-//    while (1) {
-//		SendStr(prompt); // Vyslani vyzvy k zadani loginu
+    beep();
 
-//		for (n = 0; n < 8; n++) {
-//			c = ReceiveCh();
-//			SendCh(c); // Prijaty znak se hned vysle - echo linky
-//			login[n] = c; // Postupne se uklada do pole
-//		}
+    while (1);
 
-//		beep(); // potvrzeni pipnutim
-//		SendStr("\r\n");
-    	//beep();
-//        Music(1);
-//   	}
-    PlaySong1();
-
-    while(1);
 }
