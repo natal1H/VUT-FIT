e8e90ba94b4f2f3ae17c7fc91f100224e942bc93
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2019-12-16 22:07:26 +0100

    UART

--- main.c
+++ main.c
@@ -16,7 +16,7 @@
 #define GPIO_PIN_MASK 0x1Fu
 #define GPIO_PIN(x) (((1)<<(x & GPIO_PIN_MASK)))
 #define SPK 0x10          // Speaker is on PTA4
-
+#define TIME_SCALE 2500
 #define BUS_CLOCK_FREQ 50000000 // 50MHz
 
 enum Tones {
@@ -53,20 +53,48 @@ void MCUInit(void) {
 	WDOG_STCTRLH &= ~WDOG_STCTRLH_WDOGEN_MASK;
 }
 
-/* Povoleni hodin do modulu, ktere budou pouzivany */
-void SysInit(void) {
-	// Defaultne je FTM clocked by internal bus clock
-	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK; // povoleni hodin do portu B (pro RGB LED)
-	SIM->SCGC6 |= SIM_SCGC6_FTM0_MASK; // povoleni hodin do casovace FTM0
-}
-
 void PortsInit(void)
 {
 	/* Turn on all port clocks */
-	SIM->SCGC5 = SIM_SCGC5_PORTA_MASK;
+	SIM->SCGC5 |= (SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTE_MASK);
+	SIM->SCGC6 |= SIM_SCGC6_FTM0_MASK; // povoleni hodin do casovace FTM0
+	SIM->SCGC1 |= SIM_SCGC1_UART5_MASK;
+
+	PORTE->PCR[8] = (0 | PORT_PCR_MUX(0x03));
+	PORTE->PCR[9] = (0 | PORT_PCR_MUX(0x03));
 
 	PORTA->PCR[4] = PORT_PCR_MUX(0x03);  // Speaker
 	PTA->PDDR = GPIO_PDDR_PDD(SPK);     // Speaker as output
+}
+
+void UART5Init() {
+	UART5->C2 &= ~(UART_C2_TE_MASK | UART_C2_RE_MASK);
+	UART5->BDH = 0x00;
+	UART5->BDL = 0x1A; // Baud rate 115 200 Bd, 1 stop bit
+	UART5->C4 = 0x0F; // Oversampling ratio 16, match address mode disabled
+	UART5->C1 = 0x00; // 8 data bitu, bez parity
+	UART5->C3 = 0x00;
+	UART5->MA1 = 0x00; // no match address (mode disabled in C4)
+	UART5->MA2 = 0x00; // no match address (mode disabled in C4)
+	UART5->S2 |= 0xC0; // ?
+	UART5->C2 |= (UART_C2_TE_MASK | UART_C2_RE_MASK); // ?
+}
+
+void SendCh(char ch) {
+	while (!(UART5->S1 & UART_S1_TDRE_MASK) && !(UART5->S1 & UART_S1_TC_MASK));
+	UART5->D = ch;
+}
+
+char ReceiveCh(void) {
+	while (!(UART5->S1 & UART_S1_RDRF_MASK));
+	return UART5->D;
+}
+
+void SendStr(char *s) {
+	int i = 0;
+	while (s[i] != 0) {
+		SendCh(s[i++]);
+	}
 }
 
 int getMOD(int freq, int prescale) {
@@ -76,10 +104,6 @@ int getMOD(int freq, int prescale) {
 
 int getCxV(int MOD) {
 	return (MOD + 1) / 2;
-}
-
-void FTM0_IRQHandler(void) {
-
 }
 
 void TimerInit(int freq) {
@@ -125,7 +149,7 @@ int getTOF() {
  */
 void playNote(int tone, int duratiom) {
 	int count = 0;
-	int count_end = ((tone * duratiom) / 1000) * 2500;
+	int count_end = ((tone * duratiom) / 1000) * TIME_SCALE;
 
 	TimerInit(tone);
 
@@ -136,18 +160,105 @@ void playNote(int tone, int duratiom) {
 	}
 
 	TImerTurnOff();
+	delay(100000);
+}
+
+unsigned char prompt[14] = "\rUART_Tx_Rdy\n";
+
+void displayHeader(void) {
+	SendStr("\r- Simple electronic piano -\n");
+	SendStr("\r===========================\n");
+}
+
+void displayOptionsMain(void) {
+	SendStr("\rChoose action:\n");
+	SendStr("\r1. Play prepared song\n");
+	SendStr("\r2. Play your own song\n");
+	SendStr("\r3. Quit\n");
+	SendStr("\rYour answer (1/2/3): ");
+}
+
+void playSong1(void) {
+	playNote(E4, 500);
+	playNote(G4, 500);
+	playNote(A4, 1000);
+
+	delay(500 * TIME_SCALE);
+
+	playNote(E4, 500);
+	playNote(G4, 500);
+	playNote(B4, 300);
+	playNote(A4, 1000);
+
+	delay(500 * TIME_SCALE);
+
+	playNote(E4, 500);
+	playNote(G4, 500);
+	playNote(A4, 1000);
+	playNote(G4, 500);
+	playNote(E4, 500);
+}
+
+void actionPlayPreparedSong(void) {
+
+}
+
+void actionPlayOwnSong(void) {
+
+}
+
+int determineAction(void) {
+	displayHeader();
+	displayOptionsMain();
+
+	char answer = ReceiveCh();
+	SendCh(answer);
+
+	while (1) {
+		if (answer == '1') {
+			SendStr("\n\rYou chose to play prepared song.\n");
+
+			actionPlayPreparedSong();
+		}
+		else if (answer == '2') {
+			SendStr("\n\rYou chose to play your own song.\n");
+
+			actionPlayOwnSong();
+		}
+		else if (answer == '3') {
+			// "Quit"
+			SendStr("\n\rQuiting...\n\r");
+			break;
+		}
+		else {
+			// Repeat asking
+			SendStr("\n\rWrong answer, choose again (1/2/3): ");
+			answer = ReceiveCh();
+			SendCh(answer);
+			continue;
+		}
+
+		displayOptionsMain();
+		answer = ReceiveCh();
+		SendCh(answer);
+	}
+
+	return 0;
 }
 
 int main(void) {
 	int count = 0;
 
 	MCUInit();
-	SysInit();
 	PortsInit();
+	UART5Init();
+
+	//determineAction();
+
+	playSong1();
 
 	while (1) {
-		playNote(C5, 2000);
-		delay(100000);
-		playNote(A4, 2000);
+		//playNote(C5, 2000);
+		//playNote(A4, 500);
 	}
 }
