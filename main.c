a9623c04ffd0882bc132297da8bc6ece461b6e26
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2019-12-22 12:39:23 +0100

    Update

--- main.c
+++ main.c
@@ -13,11 +13,13 @@
 #include <stdio.h>
 
 /* Macros for bit-level registers manipulation */
-#define GPIO_PIN_MASK 0x1Fu
-#define GPIO_PIN(x) (((1)<<(x & GPIO_PIN_MASK)))
 #define SPK 0x10          // Speaker is on PTA4
 #define TIME_SCALE 2500
+#define TIME_DELAY_SCALE 2500
 #define BUS_CLOCK_FREQ 50000000 // 50MHz
+#define PRESCALE 8
+
+int curCnV;
 
 enum Tones {
 	C4 = 261,
@@ -37,15 +39,6 @@ void delay(long long bound) {
 	for (i = 0; i < bound; i++);
 }
 
-void beep(void) {
-	for (unsigned int q = 0; q < 500; q++) {
-		PTA->PDOR = GPIO_PDOR_PDO(0x0010);
-		delay(500);
-		PTA->PDOR = GPIO_PDOR_PDO(0x0000);
-		delay(500);
-	}
-}
-
 /* Initialize the MCU - basic clock settings, turning the watchdog off */
 void MCUInit(void) {
 	MCG_C4 |= (MCG_C4_DMX32_MASK | MCG_C4_DRST_DRS(0x01));
@@ -53,8 +46,7 @@ void MCUInit(void) {
 	WDOG_STCTRLH &= ~WDOG_STCTRLH_WDOGEN_MASK;
 }
 
-void PortsInit(void)
-{
+void PortsInit(void) {
 	/* Turn on all port clocks */
 	SIM->SCGC5 |= (SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTE_MASK);
 	SIM->SCGC6 |= SIM_SCGC6_FTM0_MASK; // povoleni hodin do casovace FTM0
@@ -77,13 +69,14 @@ void UART5Init() {
 	UART5->MA1 = 0x00; // no match address (mode disabled in C4)
 	UART5->MA2 = 0x00; // no match address (mode disabled in C4)
 	UART5->S2 |= 0xC0; // ?
-	UART5->C2 |= (UART_C2_TE_MASK | UART_C2_RE_MASK); // ?
+	UART5->C2 |= (UART_C2_TE_MASK | UART_C2_RE_MASK);
 }
 
 void SendCh(char ch) {
 	while (!(UART5->S1 & UART_S1_TDRE_MASK) && !(UART5->S1 & UART_S1_TC_MASK));
 	UART5->D = ch;
 }
+
 
 char ReceiveCh(void) {
 	while (!(UART5->S1 & UART_S1_RDRF_MASK));
@@ -97,47 +90,37 @@ void SendStr(char *s) {
 	}
 }
 
-int getMOD(int freq, int prescale) {
-	int tmp = BUS_CLOCK_FREQ / prescale;
-	//return (tmp - freq) / freq;
-	return tmp / freq;
+int getMOD(int freq) {
+	int tmp = BUS_CLOCK_FREQ / PRESCALE;
+	return (tmp - freq) / freq;
 }
 
 int getCxV(int MOD) {
-	//return (MOD + 1) / 2;
-	return MOD / 2;
+	return (MOD + 1) / 2;
+}
+
+int getCnVChangeStep(int freq, int count_end) {
+	int CnV = getCxV(getMOD(freq));
+	int half_time = count_end / 2;
+
+	return half_time / CnV;
 }
 
 void TimerInit(int freq) {
-	int prescale = 8;
-
 	// Vynulova register citaca
 	FTM0_CNT = 0x0;
 
-	//FTM0_CNTIN = 0x0;
-
 	// Nastavi hodnotu preteèenia do modulo registru FTMO
-	FTM0_MOD = getMOD(freq, prescale);
-	//FTM0_MOD = 13636;
-
-	char mod_str[20];
-	sprintf(mod_str, "\r%d\n", getMOD(freq, prescale));
-	SendStr(mod_str);
+	FTM0_MOD = getMOD(freq);
 
 	// Nastavi režim generovania PWM na zvolenom kanale (n) casovaca
 	// v riadiacom registri FTMx_CnSC
 	// Edge-aligned PWM: High-true pulses (clear Output on match, set Output on reload),
 	// prerusenie ani DMA requests nebudu vyuzivane.
-	//FTM0_C1SC = 0x28;
-	FTM0_C1SC = 0x27;
+	FTM0_C1SC = 0x28;
 
 	// Nastavenie stridy (duty cycle)
 	FTM0_C1V = getCxV(FTM0_MOD);
-	//FTM0_C1V = FTM0_MOD / 2;
-
-	//char cv_str[20];
-	//sprintf(cv_str, "\r%d\n", getCxV(FTM0_MOD));
-	//SendStr(cv_str);
 
 	// Nastavi konfiguraciu casovaca v jeho stavovem a riadiacom registri (SC):
 	// (up counting mode pre Edge-aligned PWM, Clock Mode Selection (01),
@@ -165,12 +148,19 @@ int getTOF() {
 void playNote(int tone, int duratiom) {
 	int count = 0;
 	int count_end = ((tone * duratiom) / 1000) * TIME_SCALE;
+	int half_time = count_end / 2;
 
 	TimerInit(tone);
 
 	while (count < count_end) {
 		if (getTOF() == 1) { // TOF - Timer Overflow Flag
 			count++;
+		}
+
+		if (count >= half_time) {
+			// Doznievanie
+			if (FTM0_C1V > 0)
+				FTM0_C1V -= 1;
 		}
 	}
 
@@ -198,28 +188,143 @@ void playSong1(void) {
 	playNote(G4, 500);
 	playNote(A4, 1000);
 
-	delay(500 * TIME_SCALE);
+	delay(500 * TIME_DELAY_SCALE);
 
 	playNote(E4, 500);
 	playNote(G4, 500);
 	playNote(B4, 300);
 	playNote(A4, 1000);
 
-	delay(500 * TIME_SCALE);
+	delay(500 * TIME_DELAY_SCALE);
 
 	playNote(E4, 500);
 	playNote(G4, 500);
 	playNote(A4, 1000);
 	playNote(G4, 500);
+	playNote(E4, 1000);
+
+	delay(1000 * TIME_DELAY_SCALE);
+
+}
+
+void playSong2(void) {
+	playNote(C4, 500);
+	playNote(D4, 500);
 	playNote(E4, 500);
+	playNote(F4, 500);
+	playNote(G4, 500);
+	playNote(A4, 500);
+	playNote(B4, 500);
+	playNote(C5, 500);
+
+	delay(1000 * TIME_DELAY_SCALE);
+
+	playNote(C5, 500);
+	playNote(B4, 500);
+	playNote(A4, 500);
+	playNote(G4, 500);
+	playNote(F4, 500);
+	playNote(E4, 500);
+	playNote(D4, 500);
+	playNote(C4, 500);
+
+	delay(1000 * TIME_DELAY_SCALE);
+}
+
+void displaypreparedSongsOptions() {
+	SendStr("\rChoose song:\n");
+	SendStr("\r1. \"Smoke on the water\"\n");
+	SendStr("\r2. Range test\n");
+	SendStr("\r3. Back\n");
+	SendStr("\rYour answer (1/2/3): ");
 }
 
 void actionPlayPreparedSong(void) {
+	displaypreparedSongsOptions();
+
+	char answer = ReceiveCh();
+	SendCh(answer);
+
+	while (1) {
+		if (answer == '1') {
+			SendStr("\n\rYou chose to song 1.\n");
+			playSong1();
+			break;
+		}
+		else if (answer == '2') {
+			SendStr("\n\rYou chose to song 2.\n");
+			playSong2();
+			break;
+		}
+		else if (answer == '3') {
+			// "Back"
+			SendStr("\n\rBack...\n\r");
+			break;
+
+		}
+		else {
+			// Repeat asking
+			SendStr("\n\rWrong answer, choose again (1/2/3): ");
+			answer = ReceiveCh();
+			SendCh(answer);
+			continue;
+		}
+
+		displaypreparedSongsOptions();
+		answer = ReceiveCh();
+		SendCh(answer);
+	}
+}
+
+void displayOwnSongOptions(void) {
+	SendStr("\rInput style options:\n");
+	SendStr("\r1. As string (note, duration)\n");
+	SendStr("\r2. Keyboard press (fixed duration)\n");
+	SendStr("\r3. Back\n");
+	SendStr("\rYour answer (1/2/3): ");
+}
+
+void actionPlayInputString(void) {
 
 }
 
-void actionPlayOwnSong(void) {
+void actionPlayInputKeyboardFixed(void) {
 
+}
+
+
+void actionPlayOwnSong(void) {
+	displayOwnSongOptions();
+
+	char answer = ReceiveCh();
+	SendCh(answer);
+
+	while (1) {
+		if (answer == '1') {
+			SendStr("\n\rYou chose input as string.\n");
+			actionPlayInputString();
+		}
+		else if (answer == '2') {
+			SendStr("\n\rYou chose keyboard input (fixed duration).\n");
+			actionPlayInputKeyboardFixed();
+		}
+		else if (answer == '3') {
+			// "Quit"
+			SendStr("\n\rBack...\n\r");
+			break;
+		}
+		else {
+			// Repeat asking
+			SendStr("\n\rWrong answer, choose again (1/2/3): ");
+			answer = ReceiveCh();
+			SendCh(answer);
+			continue;
+		}
+
+		displayOwnSongOptions();
+		answer = ReceiveCh();
+		SendCh(answer);
+	}
 }
 
 int determineAction(void) {
@@ -261,30 +366,25 @@ int determineAction(void) {
 	return 0;
 }
 
-void Timer0Init(void) {
-	FTM0_CNT = 0;
-	FTM0_MOD = 22900;
-	FTM0_C1SC = 0x27;
-	FTM0_SC = 0xB;
-	FTM0_C1V = FTM0_MOD / 2;
-}
 
 int main(void) {
 	int count = 0;
 
 	MCUInit();
 	PortsInit();
-	//Timer0Init();
 	UART5Init();
 
-	//determineAction();
+	determineAction();
 
 	//playSong1();
 
-	TimerInit(A4);
+	//TimerInit(A4);
 
 	while (1) {
-		//playNote(C5, 2000);
-		//playNote(A4, 500);
+		//playNote(C5, 1000);
+
+		//delay(500 * TIME_SCALE);
+
+		//playNote(A4, 1000);
 	}
 }
